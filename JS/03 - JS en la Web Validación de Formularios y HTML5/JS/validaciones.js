e0484ef5b69d9cd27c3142273b3feb002712a538
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-12-19 07:14:53 -0600

    Se  creo una validaxión sobre no escribir en los regustros

--- JS/03 - JS en la Web Validación de Formularios y HTML5/JS/validaciones.js
+++ JS/03 - JS en la Web Validación de Formularios y HTML5/JS/validaciones.js
@@ -6,13 +6,51 @@ export function valida(input) {
     console.log(input.parentElement);
     if(input.validity.valid){
         input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHtml = ""
     } else {
         input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHtml = mostrarMensajeDeError(tipoDeInput, input);
+    }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patterMismatch" ,
+    "customError"   
+]
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "Este campo no puede estar vacio"
+    },
+    email: {
+        valueMissing: "Este campo no puede estar vacio",
+        typeMismatch: "El correo no es valido"
+    },
+    password: {
+        valueMissing: "Este campo no puede estar vacio",
+        patterMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales."
+    },
+    nacimiento: {
+        valueMissing: "Este campo no puede estar vacio",
+        customError: "Debes tener al menos 18 años"
     }
 }
 
 const validadores = {
     nacimiento: (input) => validarNacimiento(input),
+}
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = ""
+    tipoDeErrores.forEach( error => {
+        if(input.validity[error]){
+            console.log(input.validity[error]);
+            mensaje = mensajesDeError[tipoDeInput][error]
+        }
+    })
+    return mensaje;
 }
 
 function validarNacimiento(input) {
