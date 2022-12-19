e47a48f9051290f45c963fc846b65042a65c34fd
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-12-19 11:22:03 -0600

    Se termino las validaciones para formulario, muy interesante por cierto

--- JS/03 - JS en la Web Validación de Formularios y HTML5/JS/app.js
+++ JS/03 - JS en la Web Validación de Formularios y HTML5/JS/app.js
@@ -2,8 +2,8 @@ import { valida } from "./validaciones.js";
 
 const inputs = document.querySelectorAll("input");
 
-inputs.forEach( input => {
-    input.addEventListener('blur', (input) => {
-        valida(input.target);
-    })
-})
+inputs.forEach((input) => {
+  input.addEventListener("blur", (input) => {
+    valida(input.target);
+  });
+});
